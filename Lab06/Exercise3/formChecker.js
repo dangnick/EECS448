6791ccfae9dd66da13e247bf4b0b643f86b6bc44
dangnick <58992608+dangnick@users.noreply.github.com>
2022-11-13 22:04:57 -0600

    Create formChecker.js

---
+++ Lab06/Exercise3/formChecker.js
@@ -0,0 +1,51 @@
+function validateForm() {
+
+        var x = document.forms["cart"]["cool"].value;
+        if (x == null || x == "" || x <0)
+        {
+                alert("Item quantities cannot be blank or negative");
+                return false;
+        }
+
+        var y = document.forms["cart"]["shades"].value;
+        if (y == null || y == "" || y<0)
+        {
+                alert("Item quantities cannot be blank or negative");
+                return false;
+        }
+
+        var z = document.forms["cart"]["boss"].value;
+        if (z == null || z == "" || z<0)
+        {
+                alert("Item quantities cannot be blank or negative");
+                return false;
+        }
+
+        var three = document.getElementById("three").checked;
+        var overnight = document.getElementById("overnight").checked;
+        var free = document.getElementById("free").checked;
+        if(!three && !overnight && !free)
+        {
+                alert("A shipping option must be selected");
+                return false;
+        }
+
+        var username = document.forms["cart"]["username"].value;
+
+
+        var regex = /^([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)@([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)[\\.]>
+
+        if(!regex.test(username))
+        {
+                alert("Username must be a valid email.");
+                return false;
+        }
+
+        var password = document.forms["cart"]["password"].value;
+        if(password == null || password == "")
+        {
+                alert("Password cannot be blank");
+                return false;
+        }
+
+}
